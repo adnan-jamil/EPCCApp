0b018a3767d5fbddc40eefc567ec590edd47d570
Muhammad Zubair <zubair.virtuenetz@gmail.com>
2026-05-26 18:00:29 +0500

    Update app icons and iOS project settings
    Replace Android launcher icons and add/update iOS AppIcon assets across multiple sizes; adjust iOS project and plist settings to ensure correct asset catalog inclusion and runtime behavior. Changes in Xcode project: set ASSETCATALOG_COMPILER_INCLUDE_ALL_APPICON_ASSETS to AppIcon and add empty input/output paths for the Embed Pods Frameworks phase. Info.plist: add CADisableMinimumFrameDurationOnPhone and UIApplicationSupportsIndirectInputEvents keys and reorganize supported interface orientations. Code changes: hide Constant from cloud_firestore imports in profile and admin home to avoid symbol conflicts. pubspec.yaml: configure flutter_icons for iOS (remove_alpha_ios: true and background_color_ios: "#FFFFFF").

--- lib/Screens/profile.dart
+++ lib/Screens/profile.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart' hide Constant;
 import 'package:epcc/Authentication/DBService.dart';
 import 'package:epcc/Authentication/authentication.dart';
 import 'package:epcc/Bindings/HomePageBinding.dart';
--- lib/admin/views/admin_home.dart
+++ lib/admin/views/admin_home.dart
@@ -1,5 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart' hide Constant;
 import 'package:epcc/Models/constants.dart';
 import 'package:epcc/controllers/profileController.dart';
 import 'package:flutter/cupertino.dart';
